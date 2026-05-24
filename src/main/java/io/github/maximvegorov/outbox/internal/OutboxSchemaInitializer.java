0d83e11067f5e41cab745d8f120f3b01c910da54
Maxim Egorov <maxim.v.egorov@gmail.com>
2026-05-24 23:25:59 +0300

    Add log while initializing schema

--- src/main/java/io/github/maximvegorov/outbox/internal/OutboxSchemaInitializer.java
+++ src/main/java/io/github/maximvegorov/outbox/internal/OutboxSchemaInitializer.java
@@ -1,6 +1,7 @@
 package io.github.maximvegorov.outbox.internal;
 
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.InitializingBean;
 import org.springframework.core.io.ClassPathResource;
 import org.springframework.jdbc.core.JdbcTemplate;
@@ -9,15 +10,18 @@ import org.springframework.jdbc.datasource.init.ScriptUtils;
 import java.sql.Connection;
 
 @RequiredArgsConstructor
+@Slf4j
 public class OutboxSchemaInitializer implements InitializingBean {
     private final JdbcTemplate jdbcTemplate;
 
     @Override
-    public void afterPropertiesSet() throws Exception {
+    public void afterPropertiesSet() {
+        log.info("Initializing outbox schema");
         var schema = new ClassPathResource("io/github/maximvegorov/outbox/schema.sql");
         jdbcTemplate.execute((Connection connection) -> {
             ScriptUtils.executeSqlScript(connection, schema);
             return null;
         });
+        log.info("Completed outbox schema initialization");
     }
 }
